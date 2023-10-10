a713611bd36ebe3fbbc522fcc1eb5d0f5013131e
nadiatimoleon <102558021+nadiatimoleon@users.noreply.github.com>
2023-10-10 23:45:18 +0200

    some bug fixes in the std pipeline

--- pipeline_std.py
+++ pipeline_std.py
@@ -10,13 +10,17 @@ img_fn = "C:/Users/Nadia Timoleon/Documents/GitHub/ImageText-to-Speech/scene_tex
 # load image
 image = Image.open(img_fn)
 
-# loading model
-reader = easyocr.Reader(['en'], gpu=False)
-
 # scene text detection from image
+reader = easyocr.Reader(['en'], gpu=False)
 result = reader.readtext(img_fn)
-generated_text = ' '.join([result[i][1] for i in range(len(result))])
+
+# sorting text from top to bottom based on bounding box upper left point
+text = [result[i][1] for i in range(len(result))]
+upper_left_point = [result[i][0][0] for i in range(len(result))]
+sorted_text = [x for x, _ in sorted(zip(text, upper_left_point), key=lambda x: x[1][1])]
+
+generated_text = ' '.join([t for t in sorted_text if t != ''])
 print("detected text:", generated_text)
 
 # text to speech processing
-get_speech(generated_text, "handwritten_audio.mp3")
+get_speech(generated_text, "scene_text_audio.mp3")
