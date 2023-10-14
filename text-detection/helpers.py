36a575ebe02e7e5306dc4155d9e8cab8649193bd
Rupal Saxena <rupal.saxena@uzh.ch>
2023-10-14 17:42:34 +0200

    seperated text detection algos

--- text-detection/helpers.py
+++
@@ -1,8 +0,0 @@
-def compute_cer(cer_metric, processor, pred_ids, label_ids):
-    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
-    label_ids[label_ids == -100] = processor.tokenizer.pad_token_id
-    label_str = processor.batch_decode(label_ids, skip_special_tokens=True)
-
-    cer = cer_metric.compute(predictions=pred_str, references=label_str)
-
-    return cer
